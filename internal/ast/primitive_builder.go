52e6ce7b406cf36cbfaeed6b2504f06920317684
Sam L <samlitowitz@users.noreply.github.com>
2024-01-01 14:49:56 -0500

    Add support and unit tests for files

--- internal/ast/primitive_builder.go
+++ internal/ast/primitive_builder.go
@@ -13,6 +13,10 @@ type PrimitiveBuilder struct {
 	moduleRootDirectory string
 
 	packagesByDirName map[string]*internal.Package
+	filesByAbsPath    map[string]*internal.File
+
+	curPkg  *internal.Package
+	curFile *internal.File
 }
 
 func NewPrimitiveBuilder(modulePath, moduleRootDirectory string) *PrimitiveBuilder {
@@ -21,34 +25,52 @@ func NewPrimitiveBuilder(modulePath, moduleRootDirectory string) *PrimitiveBuild
 		moduleRootDirectory: moduleRootDirectory,
 
 		packagesByDirName: make(map[string]*internal.Package),
+		filesByAbsPath:    make(map[string]*internal.File),
 	}
 }
 
 func (builder *PrimitiveBuilder) AddNode(node ast.Node) error {
 	switch node := node.(type) {
 	case *Package:
-		builder.packagesByDirName[node.DirName] = buildPackage(
-			builder.moduleRootDirectory,
-			node.DirName,
-			node.Name,
-			len(node.Files),
-		)
+		if _, ok := builder.packagesByDirName[node.DirName]; !ok {
+			builder.packagesByDirName[node.DirName] = buildPackage(
+				builder.moduleRootDirectory,
+				node.DirName,
+				node.Name,
+				len(node.Files),
+			)
+		}
+		builder.curPkg = builder.packagesByDirName[node.DirName]
 
-		//case *File:
-		//
-		//	file := &internal.File{
-		//		Package:      nil,
-		//		FileName:     "",
-		//		AbsPath:      "",
-		//		Imports:      nil,
-		//		TypesDefined: nil,
-		//	}
+	case *File:
+		if builder.curPkg == nil {
+			// return custom error, undefined package
+		}
+		if _, ok := builder.filesByAbsPath[node.AbsPath]; !ok {
+			builder.filesByAbsPath[node.AbsPath] = &internal.File{
+				Package:      builder.packagesByDirName[node.DirName],
+				FileName:     filepath.Base(node.AbsPath),
+				AbsPath:      node.AbsPath,
+				Imports:      make(map[string]*internal.Import),
+				TypesDefined: make(map[string]*internal.Type),
+			}
+		}
+		builder.packagesByDirName[node.DirName].Files[node.AbsPath] = builder.filesByAbsPath[node.AbsPath]
+		builder.curFile = builder.filesByAbsPath[node.AbsPath]
 	}
 
 	return nil
 }
 
-func (builder PrimitiveBuilder) Packages() []*internal.Package {
+func (builder *PrimitiveBuilder) Files() []*internal.File {
+	files := make([]*internal.File, 0, len(builder.filesByAbsPath))
+	for _, file := range builder.filesByAbsPath {
+		files = append(files, file)
+	}
+	return files
+}
+
+func (builder *PrimitiveBuilder) Packages() []*internal.Package {
 	pkgs := make([]*internal.Package, 0, len(builder.packagesByDirName))
 	for _, pkg := range builder.packagesByDirName {
 		pkgs = append(pkgs, pkg)
