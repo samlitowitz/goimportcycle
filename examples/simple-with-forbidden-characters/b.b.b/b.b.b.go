3ca50a7e3a775ff68a9517df306ff5079e3ab94e
kaiiorg <k349@live.com>
2024-01-15 18:11:02 -0600

    Fix forbidden characters example

--- examples/simple-with-forbidden-characters/b.b.b/b.b.b.go
+++ examples/simple-with-forbidden-characters/b.b.b/b.b.b.go
@@ -3,10 +3,10 @@ package b
 import (
 	"log"
 
-	"github.com/samlitowitz/goimportcycle/examples/simple-with-forbidden-characters/a_a_a"
+	a "github.com/samlitowitz/goimportcycle/examples/simple-with-forbidden-characters/a_a_a"
 )
 
 func Fn() {
-	a_a_a.Fn()
+	a.Fn()
 	log.Println("B")
 }
