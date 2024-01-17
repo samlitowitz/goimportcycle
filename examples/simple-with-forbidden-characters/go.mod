4d127549d1eff7ec3547a34e2e994890e0c7ba4f
Sam L <samlitowitz@users.noreply.github.com>
2024-01-16 21:07:25 -0500

    Update examples and their outputs

--- examples/simple-with-forbidden-characters/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/samlitowitz/goimportcycle/examples/simple
-
-go 1.21.5
--- examples/simple-with-forbidden-characters/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import a_a_a "github.com/samlitowitz/goimportcycle/examples/simple/a-a-a"
-
-func main() {
-	a_a_a.Fn()
-}
